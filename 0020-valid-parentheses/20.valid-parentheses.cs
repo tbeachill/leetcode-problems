44a0022a304cd8c019f4fb384c9a0fd2b00e28d9
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-08-08 03:20:19 +0100

    finished problem

---
+++ 0020-valid-parentheses/20.valid-parentheses.cs
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=20 lang=csharp
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+public class Solution {
+    public bool IsValid(string s) {
+        var parDict = new Dictionary<char, char>(){
+            {'(', ')'},
+            {'[', ']'},
+            {'{', '}'}
+        };
+        Stack<char> parStack = new Stack<char>();
+
+        foreach (char c in s)
+        {
+            if ("([{".Contains(c))
+            {
+                parStack.Push(c);
+            }
+            else if (")]}".Contains(c) && parStack.Count > 0)
+            {
+                if (parDict[parStack.Pop()] != c)
+                    return false;
+            }
+            else return false;
+        }
+
+        if (parStack.Count > 0) return false;
+        else return true;
+    }
+}
+// @lc code=end
+
