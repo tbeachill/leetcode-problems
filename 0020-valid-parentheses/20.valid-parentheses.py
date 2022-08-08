b4cebea38f033ba1d735e60a9f315e70a997b733
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-08-08 02:44:09 +0100

    finished problem

---
+++ 0020-valid-parentheses/20.valid-parentheses.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+
+# @lc code=start
+class Solution:
+    def isValid(self, s: str) -> bool:
+        par_dict = {
+            '(' : ')',
+            '[' : ']',
+            '{' : '}',
+        }
+        par_stack = []
+
+        for char in s:
+            if char in "([{":
+                par_stack.append(char)
+            elif char in ")]}" and len(par_stack) > 0:
+                if par_dict[par_stack.pop()] != char:
+                    return False
+            else:
+                return False
+
+        if len(par_stack) > 0:
+            return False
+        else:
+            return True
+# @lc code=end
+
