06aced1e39f0cdfac802f14bbfe5fec63f028852
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-08-15 17:32:58 +0100

    finished problem

---
+++ 0183-customers-who-never-order/183.customers-who-never-order.sql
@@ -0,0 +1,16 @@
+--
+-- @lc app=leetcode id=183 lang=mysql
+--
+-- [183] Customers Who Never Order
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+SELECT name AS Customers
+FROM Customers c
+WHERE c.id NOT IN (SELECT customerId
+                   FROM Orders);
+
+-- @lc code=end
+
