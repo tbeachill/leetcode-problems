92f963e5228dff873b28ae960a67c8138151c66b
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-08-17 18:19:51 +0100

    finished problem

---
+++ 0027-remove-element/27.remove-element.rs
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=27 lang=rust
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        let mut i = 0;
+        while i < nums.len() {
+            if nums[i] == val {
+                nums.remove(i);
+            } else {
+                i += 1;
+            }
+        }
+
+        return nums.len() as i32;
+    }
+}
+// @lc code=end
+
