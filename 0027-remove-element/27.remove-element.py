609f0de6a4c5266fed7ff8aad2ba9354f4e21208
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-08-17 17:23:43 +0100

    finished problem

---
+++ 0027-remove-element/27.remove-element.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=27 lang=python3
+#
+# [27] Remove Element
+#
+
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        while val in nums:
+            nums.remove(val)
+
+        return len(nums)
+# @lc code=end
+
