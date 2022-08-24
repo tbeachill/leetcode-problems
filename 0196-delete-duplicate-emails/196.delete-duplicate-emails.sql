3e6206e2525fd5a968fe8331cbaa83b9a0d23aef
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-08-24 19:28:01 +0100

    finished problem

---
+++ 0196-delete-duplicate-emails/196.delete-duplicate-emails.sql
@@ -0,0 +1,19 @@
+--
+-- @lc app=leetcode id=196 lang=mysql
+--
+-- [196] Delete Duplicate Emails
+--
+
+-- @lc code=start
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+
+DELETE p1
+FROM Person p1
+INNER JOIN Person p2
+WHERE
+    p1.id > p2.id AND
+    p1.email = p2.email;
+
+-- @lc code=end
+
