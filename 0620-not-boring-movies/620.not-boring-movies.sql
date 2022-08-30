19b1f340ba1117adb0a866ca571d2d2e1c7a2782
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-08-30 18:19:20 +0100

    finished problem

---
+++ 0620-not-boring-movies/620.not-boring-movies.sql
@@ -0,0 +1,17 @@
+--
+-- @lc app=leetcode id=620 lang=mysql
+--
+-- [620] Not Boring Movies
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+  SELECT *
+    FROM Cinema
+   WHERE id % 2 = 1
+     AND description != "boring"
+ORDER BY rating DESC;
+
+-- @lc code=end
+
