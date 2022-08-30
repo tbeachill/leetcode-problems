471c1cc772522b3fe0c5b72b1f4b7b4c2da7a2eb
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-08-30 18:42:49 +0100

    finished problem

---
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050.actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -0,0 +1,18 @@
+--
+-- @lc app=leetcode id=1050 lang=mysql
+--
+-- [1050] Actors and Directors Who Cooperated At Least Three Times
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+  SELECT actor_id,
+         director_id
+    FROM ActorDirector
+GROUP BY actor_id,
+         director_id
+  HAVING COUNT(*) >= 3;
+
+-- @lc code=end
+
