711b22eecf41d025cbde0aaf56f530a6b2fd563b
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-08-30 17:16:09 +0100

    finished problem

---
+++ 0058-length-of-last-word/58.length-of-last-word.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=58 lang=python3
+#
+# [58] Length of Last Word
+#
+
+# @lc code=start
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        word_list = [word for word in s.split(" ") if word != '']
+
+        return len(word_list[len(word_list) - 1])
+        
+# @lc code=end
+
