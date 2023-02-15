f48a2a9d89c2c3d06f0913873183967bb1f05632
tbeachill <82418020+tbeachill@users.noreply.github.com>
2023-02-15 13:28:39 +0000

    finished problem

---
+++ 1527-patients-with-a-condition/1527.patients-with-a-condition.sql
@@ -0,0 +1,14 @@
+--
+-- @lc app=leetcode id=1527 lang=mysql
+--
+-- [1527] Patients With a Condition
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT *
+  FROM Patients
+ WHERE conditions LIKE 'DIAB1%'
+    OR conditions LIKE '% DIAB1%';
+-- @lc code=end
+
