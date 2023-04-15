955f71ddfd6318e15b531eb2f154aaf47879ebe4
tbeachill <82418020+tbeachill@users.noreply.github.com>
2023-04-15 22:47:46 +0100

    finished problem

---
+++ 1873-calculate-special-bonus/1873.calculate-special-bonus.sql
@@ -0,0 +1,14 @@
+--
+-- @lc app=leetcode id=1873 lang=mysql
+--
+-- [1873] Calculate Special Bonus
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+  SELECT employee_id,
+         IF(employee_id % 2 = 1 AND LEFT(name, 1) != 'M', salary, 0) AS bonus
+    FROM Employees
+ORDER BY employee_id;
+-- @lc code=end
+
