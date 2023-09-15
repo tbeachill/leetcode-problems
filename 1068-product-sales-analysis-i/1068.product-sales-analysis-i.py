3f6ad330ba161a68bce8a50ec27deffa4cbbfca9
tbeachill <tbeachill@proton.me>
2023-09-15 16:51:39 +0100

    finished problem

---
+++ 1068-product-sales-analysis-i/1068.product-sales-analysis-i.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
+    result = sales.merge(product, on='product_id')[['product_name', 'year', 'price']]
+
+    return result
